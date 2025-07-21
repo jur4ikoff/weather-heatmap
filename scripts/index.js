3556c4b441fb67fc9b51dde5a8ff188f7cf802b8
1 <ypopov2005@gmail.com>
2025-07-21 15:01:41 +0300

    after migrate on v2.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,7 @@
 async function resButtonEventClickedV1(map) {
-    // const latitude = document.getElementById("latitude").value;
-    // const longitude = document.getElementById("longitude").value;
-    // const latitude = document.getElementById("map").;
-    console.log("Test");
-    const latitude = 45.53434354;
-    const longitude = 45.53434354;
+    const center = map.getCenter()
+    const latitude = center[0];
+    const longitude = center[1];
     const scale = 10;
 
     const params = new URLSearchParams(
@@ -13,10 +10,6 @@ async function resButtonEventClickedV1(map) {
             scale: scale
         }
     )
-
-    // const bounds = map.getBounds();
-    // const topleft = bounds[0] // Шировта, долгота
-    // console.log(topleft);
 
     const response = await fetch(`api/v1/heatmap?${params.toString()}`, {
         method: "GET",
@@ -37,47 +30,22 @@ async function resButtonEventClickedV1(map) {
 
 
 
-async function initMap() {
 
-    await ymaps3.ready;
-
-    const { YMap, YMapDefaultSchemeLayer, YMapFeature } = ymaps3;
-
-    const map = new YMap(
-        document.getElementById('map'),
-        {
-            location: {
-                center: [37.588144, 55.733842],
-                zoom: 10
-            }
-        }
-    );
-
-    console.log(map.location);
-    map.addChild(new YMapDefaultSchemeLayer());
-
-    const lineStringFeature = new YMapFeature({
-        id: 'line',
-        source: 'featureSource',
-        geometry: {
-            type: 'LineString',
-            coordinates: [
-                [37.588144, 55.733842],
-                [25.329762, 55.389311]
-            ]
-        },
-        style: {
-            stroke: [{ width: 12, color: 'rgb(14, 194, 219)' }]
-        }
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init() {
+    // Создание карты.
+    var map = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [55.76, 37.64],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 16
     });
 
-    map.addChild(lineStringFeature);
-    //const test = map.state;
-    //const coords = test.center;
-
     document.getElementById("resButton").addEventListener("click", async () => resButtonEventClickedV1((map)));
-    // Подписываемся на изменения центр
-
 }
-
-initMap();
